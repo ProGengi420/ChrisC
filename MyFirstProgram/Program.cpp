a03bf90c17d66a80d893fb0c7ebc131ab585ea57
ProGengi420 <chrix88888@gmail.com>
2017-07-17 16:47:07 -0400

    added variables to code

--- MyFirstProgram/Program.cpp
+++ MyFirstProgram/Program.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main()
 {
-	cout << "I Need Healing" << endl;
+	string myName = "From Ash";
+	string advice = "Use the toaster";
+	string drawing = "best drawing";
+	char mark ='?';
+	cout << "You want my advice?" << endl;
+	cout << "My advice: " << advice << endl;
+	cout << myName << mark << endl;
+	cout << "yo you gotta create a drawing boi yo" << endl;
+	cin >> myName;
+	
 }
-
